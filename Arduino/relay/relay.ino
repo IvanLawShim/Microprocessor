48acdfd90e4466bc83c90e9e7a3a223c96433882
IvanLawShim <44210886+IvanLawShim@users.noreply.github.com>
2018-10-17 03:04:49 +0800

    Add files via upload

---
+++ Arduino/relay/relay.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Start!");
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(13,LOW);
+ delay(1000);
+ digitalWrite(13, HIGH);
+ delay(1000);
+
+}
---
+++ Arduino/test_watch/test_watch.ino
@@ -0,0 +1,14 @@
+#include <avr/wdt.h>
+
+void setup(){
+  Serial.begin(9600);
+  wdt_disable();  
+  delay(3000); 
+  wdt_enable(WDTO_2S);
+}
+
+void loop(){
+  printf("Hello");
+  delay(1000);
+  wdt_reset();
+}
